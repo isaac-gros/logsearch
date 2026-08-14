a47858cb2a51d2ad1ece32c5cfbafd4d3a8376d2
Isaac Gros <isaac.gros888@gmail.com>
2026-08-14 23:24:28 +0200

    [CLEAN] Created database.py to improve readabilty

--- back/src/settings.py
+++ back/src/settings.py
@@ -1,11 +1,10 @@
 import os
 from os.path import exists, abspath
 from dotenv import load_dotenv
-from opensearchpy import AsyncOpenSearch
 
 # Chargement des variables d'environnements
+# On vérifie que le fichier est présent
 env_file = abspath('../.env')
-
 if (exists(env_file)):
   load_dotenv(env_file)
 else:
@@ -13,19 +12,17 @@ else:
 
 # Récupération des variables d'environnement
 required_vars = ['DB_CLIENT_HOST', 'DB_CLIENT_PORT']
-db_host = os.environ.get(required_vars[0])
-db_port = os.environ.get(required_vars[1])
+env_db_host = os.environ.get(required_vars[0])
+env_db_port = os.environ.get(required_vars[1])
 
 # Vérification des variables
 if (
-  (db_host != None and db_port != '') and 
-  (db_port != None and db_port != '')
+  (env_db_host == None or env_db_host == '') and 
+  (env_db_port == None or env_db_port == '')
 ):
-  opensearch_client = AsyncOpenSearch(
-     hosts=[{"host": db_host, "port": int(db_port)}],
-     use_ssl=False,
-     verify_certs=False
-  )
-else:
   vars = ", ".join(required_vars)
   raise SystemError(f"One or more environment variables are empty. Required variables are {vars}")
+else:
+
+  # On convertit en entier le port de la variable d'environnement
+  env_db_port = int(env_db_port)
